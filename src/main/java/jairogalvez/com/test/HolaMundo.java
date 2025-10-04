47c0aaa170d2f5a9a90adacb752792eb19a42810
Jairo Galvez <jairo.jggc@gmail.com>
2025-10-04 18:46:39 -0500

    feat(init): hola mundo

---
+++ src/main/java/jairogalvez/com/test/HolaMundo.java
@@ -0,0 +1,15 @@
+package jairogalvez.com.test;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HolaMundo {
+
+    @GetMapping
+    public String holaMundo(){
+        return "hola como estas";
+    }
+
+}
---
+++ src/test/java/jairogalvez/com/test/TestApplicationTests.java
@@ -0,0 +1,13 @@
+package jairogalvez.com.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
