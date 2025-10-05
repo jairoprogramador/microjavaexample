9d74ba4528f7cd6b9d6e86f3543a5907881e9412
Jairo Galvez <jairo.jggc@gmail.com>
2025-10-05 16:16:08 -0500

    feat(app): prueba 4

--- src/main/java/jairogalvez/com/test/HolaMundo.java
+++ src/main/java/jairogalvez/com/test/HolaMundo.java
@@ -9,7 +9,7 @@ public class HolaMundo {
 
     @GetMapping
     public String holaMundo(){
-        return "hola como estas soy una nueva prueba 3";
+        return "hola como estas soy una nueva prueba 4";
     }
 
 }
