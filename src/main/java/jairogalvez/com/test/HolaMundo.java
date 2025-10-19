fb7d8601bc17f31dae538451e9b6ec5b2483e6f8
Jairo Galvez <jairo.jggc@gmail.com>
2025-10-19 08:50:40 -0500

    test(fasdeploy): supply

--- src/main/java/jairogalvez/com/test/HolaMundo.java
+++ src/main/java/jairogalvez/com/test/HolaMundo.java
@@ -9,7 +9,7 @@ public class HolaMundo {
 
     @GetMapping
     public String holaMundo(){
-        return "hola como estas soy una nueva prueba 6";
+        return "hola como estas soy una nueva prueba";
     }
 
 }
