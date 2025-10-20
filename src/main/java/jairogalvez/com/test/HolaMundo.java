498b80fda8eddea7c9d247b3c4adfd4fda6ad2eb
Jairo Galvez <jairo.jggc@gmail.com>
2025-10-19 21:58:06 -0500

    test(container): edit code

--- src/main/java/jairogalvez/com/test/HolaMundo.java
+++ src/main/java/jairogalvez/com/test/HolaMundo.java
@@ -9,7 +9,7 @@ public class HolaMundo {
 
     @GetMapping
     public String holaMundo(){
-        return "hola como estas soy una nueva prueba ddd";
+        return "hola como estas soy una nueva prueba";
     }
 
 }
